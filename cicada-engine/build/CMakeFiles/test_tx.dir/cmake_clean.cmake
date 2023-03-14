92cb66af36bc13d9eae6b9b715c0571a7342f792
szu-advtech <none>
2023-03-14 17:43:37 +0800

    Initial commit

---
+++ cicada-engine/build/CMakeFiles/test_tx.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_tx.dir/src/mica/alloc/hugetlbfs_shm.cc.o"
+  "CMakeFiles/test_tx.dir/src/mica/alloc/hugetlbfs_shm.cc.o.d"
+  "CMakeFiles/test_tx.dir/src/mica/test/test_tx.cc.o"
+  "CMakeFiles/test_tx.dir/src/mica/test/test_tx.cc.o.d"
+  "CMakeFiles/test_tx.dir/src/mica/transaction/timestamp.cc.o"
+  "CMakeFiles/test_tx.dir/src/mica/transaction/timestamp.cc.o.d"
+  "CMakeFiles/test_tx.dir/src/mica/util/cityhash/city_mod.cc.o"
+  "CMakeFiles/test_tx.dir/src/mica/util/cityhash/city_mod.cc.o.d"
+  "CMakeFiles/test_tx.dir/src/mica/util/config.cc.o"
+  "CMakeFiles/test_tx.dir/src/mica/util/config.cc.o.d"
+  "CMakeFiles/test_tx.dir/src/mica/util/lcore.cc.o"
+  "CMakeFiles/test_tx.dir/src/mica/util/lcore.cc.o.d"
+  "CMakeFiles/test_tx.dir/src/mica/util/siphash/siphash24.c.o"
+  "CMakeFiles/test_tx.dir/src/mica/util/siphash/siphash24.c.o.d"
+  "CMakeFiles/test_tx.dir/src/mica/util/stopwatch.cc.o"
+  "CMakeFiles/test_tx.dir/src/mica/util/stopwatch.cc.o.d"
+  "CMakeFiles/test_tx.dir/src/mica/util/zipf.cc.o"
+  "CMakeFiles/test_tx.dir/src/mica/util/zipf.cc.o.d"
+  "test_tx"
+  "test_tx.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_tx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
